3e4b257d27776f228d2b995c67423072103c9f83
Kilian Campbell <45500144+kiliancampbell@users.noreply.github.com>
2022-09-12 20:44:36 -0500

    Update blue_whale_map.R

--- blue_whale_map.R
+++ blue_whale_map.R
@@ -16,7 +16,8 @@ head(map_data)
 colnames(map_data)
 View(map_data)
 
-which(is.na(map_data$location_long), arr.ind = TRUE)
+which(is.na(map_data$location_long), 
+      arr.ind = TRUE)
 
 class(map_data$tag_local_identifier) = 'integer'
 
@@ -25,7 +26,8 @@ class(map_data$tag_local_identifier) = 'integer'
 ?register_google
 
 mapped_whale <- get_map(location = c(lon = mean(map_data$location_long),
-                                     lat = mean(map_data$location_lat)),
+                                     lat = mean(map_data$location_lat)
+                                    ),
                         zoom = 4,
                         maptype = "satellite", 
                         scale = 2,
@@ -34,28 +36,37 @@ mapped_whale <- get_map(location = c(lon = mean(map_data$location_long),
 ##Trying contour map
 
 ggmap(mapped_whale) +
-  stat_density2d(data = map_data, aes(x = location_long,
-                                      y = location_lat,
-                                      fill =  ..level..),
+  stat_density2d(data = map_data, 
+                 aes (x = location_long,
+                      y = location_lat,
+                      fill =  ..level..),
                  bins = 30,
                  h = 3,
                  n = 100,
                  alpha = 1,
                  geom = 'polygon',
-                 size = 3) + 
+                 size = 3
+                ) + 
   scale_fill_gradientn(colors = rev(brewer.pal(10, 'Spectral')),
-                       labels = comma) +
-  geom_point (data = map_data, aes(x = location_long,
-                                   y = location_lat,
-                                   color = "Whale"),
+                       labels = comma
+                      ) +
+  geom_point (data = map_data, 
+              aes(x = location_long,
+                  y = location_lat,
+                  color = "Whale"
+                 ),
               fill = 'red',
               shape = 21,
-              alpha = .2)+
+              alpha = .2
+             )+
   guides(fill = FALSE, 
          color = guide_legend(
            title = "GPS Tag",
            override.aes = list(size = 2)
          )) +
-  theme (legend.key = element_rect(fill = 'white'),axis.title.y = element_blank(), axis.title.x = element_blank()) +
+  theme (legend.key = element_rect(fill = 'white'),
+         axis.title.y = element_blank(), 
+         axis.title.x = element_blank()
+        ) +
   ggtitle(" Azores Blue Whale Spatial Distribution, 04/2009-07/2016") +
   xlab("Long") + ylab("Lat")
